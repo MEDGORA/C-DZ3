a866b73c8d606911c7034840e503c74b9ea613bf
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-11-20 17:36:57 +0300

    сделал 19

---
+++ task19/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизхначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99999 || number < 10000)
+{
+    Console.WriteLine($"Число {number} не является пятизначным");
+}
+else
+{
+    int i = 0;
+    int n = 4;
+    int a = 0; // Счётчик считает сколько пар цифр (c i-ым и n-ым индексами) совпали в числе
+    string str = number.ToString();
+    while (i != n)
+    {
+        if (str[i] == str[n])
+        {
+            a++;
+        }
+        i++;
+        n--;
+    }
+    if (a == 2)
+    {
+        Console.WriteLine($"Да, число {number} является палиндромом");
+    }
+    else 
+    {
+        Console.WriteLine($"Нет, число {number} не является палиндромом");
+    }
+}
+        
+      
